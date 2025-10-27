104d4c7c272262298b59ce8b2d5f020245d5b736
brenoctorres-alt <brenoctorres@hotmail.com>
2025-10-27 12:48:43 -0300

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,7 +9,7 @@ specifies that any user authenticated via an API key can "create", "read",
 const schema = a.schema({
   Todo: a
     .model({
-      content: a.string(),
+      content: a.string(),  }).authorization(allow => [allow.owner()]),
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
@@ -19,6 +19,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
+        defaultAuthorizationMode: 'userPool',
     defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
